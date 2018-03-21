a4be688f362456d944061d74f573163930592890
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-21 18:20:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ function nestedTarget() {
 function increaseRankBy(n) {
   let collection = document.querySelectorAll(".ranked-list")
   for (var i = 0; i < collection.length; i++) {
-    console.log(collection[i].length);
-    for (var j = 0; j < collection[i].length; j++) {
-      console.log("Hello!");
-      // collection[i].children[j].innerHTML = `${parseInt(collection[i].children[j].innerHTML,10)+n}`;
+    for (var j = 0; j < collection[i].children.length; j++) {
+      collection[i].children[j].innerHTML = `${parseInt(collection[i].children[j].innerHTML,10)+n}`;
     }
   }
 }
