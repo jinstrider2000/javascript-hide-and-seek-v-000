f303ff890d41ba6b7c1a0da28fc774c57c3103ca
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-21 18:03:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function increaseRankBy(n) {
   let collection = document.querySelectorAll(".ranked-list")
   for (var i = 0; i < collection.length; i++) {
     for (var j = 0; j < collection[i].length; j++) {
-      collection[i].children[j].innerHTML = `${parseInt(collection[i].children[j].innerHTML,10)+n}`;
+      console.log(collection[i].children[j].innerHTML);
+      // collection[i].children[j].innerHTML = `${parseInt(collection[i].children[j].innerHTML,10)+n}`;
     }
   }
 }
